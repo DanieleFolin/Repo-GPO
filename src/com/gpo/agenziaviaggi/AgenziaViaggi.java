5341559a99d1c925adccb830ba703a03153751be
DanieleFolin <71838106+DanieleFolin@users.noreply.github.com>
2021-03-31 17:59:45 +0200

    Test di commit 2

--- src/com/gpo/agenziaviaggi/AgenziaViaggi.java
+++ src/com/gpo/agenziaviaggi/AgenziaViaggi.java
@@ -2,7 +2,7 @@ package com.gpo.agenziaviaggi;
 
 public class AgenziaViaggi {
 
-    public static <String> void main(String[] args) {
+    public static void main(String[] args) {
         System.out.print(8);
     }
 }
