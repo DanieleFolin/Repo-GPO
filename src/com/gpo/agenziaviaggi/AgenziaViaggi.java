fd4bc425a4793434aad803f2e86390d4e15512c8
MarBerSpace <matteob0703@gmail.com>
2021-03-31 18:09:19 +0200

    Prova di modifica 2

--- src/com/gpo/agenziaviaggi/AgenziaViaggi.java
+++ src/com/gpo/agenziaviaggi/AgenziaViaggi.java
@@ -5,8 +5,11 @@ public class AgenziaViaggi {
     public static void main(String[] args) {
 
         int prova = 10;
-
         System.out.print(8);
+        String ciao = funzioneProva();
+    }
 
+    private static String funzioneProva(){
+        return "ciao";
     }
 }
